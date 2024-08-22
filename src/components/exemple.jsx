5eccffe0fdfabd8077256c3cc1e1230338d24853
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-08-22 13:16:01 +0300

    1

--- src/components/exemple.jsx
+++ src/components/exemple.jsx
@@ -7,7 +7,7 @@ const Example = () => {
       <h1 className="text-4xl font-bold text-blue-600 p-10">Bine ai venit!</h1>
       <p className="text-lg text-gray-700 mt-4">
         Aceasta este componenta Example stilizată complet cu Tailwind CSS.
-        Stilurile sunt aplicate în această componentă.
+        Stilurile, sunt aplicate în această componentă.
       </p>
     </div>
   );
